07146f2ab38031d10159282ee03d4a26f05b0480
vaneto0o <123696310+vaneto0o@users.noreply.github.com>
2023-02-22 15:15:44 +0200

    neew

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
+  
   const price=document.getElementsByClassName('price');
   var elements = document.getElementByClassName('product');
   for(var i=0; i < elements.length; i++){
     elements[i].setAttribure('data-price', price[0].innerHTML);
   }
-
-
 });
