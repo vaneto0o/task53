4609e1c05b49473234819448b9194bea6c04a9b1
vaneto0o <123696310+vaneto0o@users.noreply.github.com>
2023-02-22 15:32:25 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  //THis block will be executed once page is loaded and ready
+  // This block will be executed once page is loaded and ready
   
-  const price=document.getElementsByClassName('price');
+  const price = document.getElementsByClassName('price');
   var elements = document.getElementByClassName('product');
   for(var i=0; i < elements.length; i++){
     elements[i].setAttribure('data-price', price[0].innerHTML);
